810b3d2025579fbc6fb0b66db360d17e781dc150
huggybaird <huggybaird@gmail.com>
2022-01-08 13:02:03 -0500

    mdx and blog added

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,5 +22,12 @@ module.exports = {
     },
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 };
---
+++ src/pages/blog.js
@@ -0,0 +1,37 @@
+import * as React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/layout'
+
+const BlogPage = ({ data }) => {
+  return (
+    <Layout pageTitle="My Blog Posts">
+      <ul>
+      {
+        data.allFile.nodes.map(node => (
+          <li key={node.name}>
+            {node.name}
+          </li>
+        ))
+      }
+      </ul>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allFile {
+      nodes {
+        name
+        absolutePath
+        relativePath
+        relativeDirectory
+        publicURL
+        uid
+        size
+      }
+    }
+  }
+`
+
+export default BlogPage
